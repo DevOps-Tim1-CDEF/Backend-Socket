94abd8fb958a5158711bcf58c90bfafdc29e920a
Cindy Sintiya <211110347@students.mikroskil.ac.id>
2024-11-07 21:22:31 +0700

    update workflow and app.js for checking automation

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Welcome to DevOps Tim 1 - percobaan");
+  res.send("Welcome to DevOps Tim 1 - last update 07/11/2024 21:21");
 });
 app.use("/user", userRoutes);
 app.use("/thread", threadRoutes);
