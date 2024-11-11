36ae3d60c838df6dc0760c64bb4278a817b322a1
Farrell Rio Fa <92140651+R10F@users.noreply.github.com>
2024-11-11 22:18:38 +0700

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Welcome to DevOps Tim 1 - last update 07/11/2024 21:41");
+  res.send("Welcome to DevOps Tim 1 - last update 11/11/2024 22:18");
 });
 app.use("/user", userRoutes);
 app.use("/thread", threadRoutes);
