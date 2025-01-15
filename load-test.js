3a42cd33a398813e0061d446051393fe8c3f4ebe
Farrell Rio Fa <farrellriofa@gmail.com>
2025-01-15 21:08:16 +0700

    done

--- load-test.js
+++ load-test.js
@@ -2,7 +2,10 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 
 export let options = {
-  stages: [{ duration: "30s", target: 5000 }],
+  stages: [
+    { duration: "30s", target: 100 },
+    { duration: "1m", target: 500 },
+  ],
 };
 
 export default function () {
