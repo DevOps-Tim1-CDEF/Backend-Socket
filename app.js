60bb18c4f8509973aaaa5d51a38f10db77b53e06
Farrell Rio Fa <farrellriofa@gmail.com>
2025-01-15 12:44:34 +0700

    try argocd

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Welcome to DevOps Tim 1 - Testing argocd");
+  res.send("Welcome to DevOps Tim 1 - Coba lagi lah");
 });
 app.use("/user", userRoutes);
 app.use("/thread", threadRoutes);
