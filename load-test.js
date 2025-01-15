c143a8993275ec14ca7d77c24fdf13b6c86fa578
Farrell Rio Fa <farrellriofa@gmail.com>
2025-01-15 20:36:23 +0700

    done

--- load-test.js
+++ load-test.js
@@ -2,17 +2,12 @@ import http from "k6/http";
 import { check, sleep } from "k6";
 
 export let options = {
-  stages: [
-    { duration: "30s", target: 2500 },
-    { duration: "2m", target: 5000 },
-    { duration: "3m", target: 5500 },
-    { duration: "20s", target: 1500 },
-  ],
+  stages: [{ duration: "30s", target: 5000 }],
 };
 
 export default function () {
   // Replace with the actual endpoint of your Express application
-  let res = http.post("http://localhost:2500/thread"); // e.g., http://localhost:3000/
+  let res = http.post("http://localhost:2500/"); // e.g., http://localhost:3000/
 
   // Check if the response status is 200
   check(res, {
